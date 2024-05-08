19b4d2fc894555b554fde35bf9849459871dda31
sandra rivas <srivas@outlook.es>
2024-05-08 19:45:38 +0200

    Primera Version

--- src/javadoca/aritmetica.java
+++ src/javadoca/aritmetica.java
@@ -4,24 +4,52 @@ package javadoca;
 /**
  *
  * @author claramolinosaraiz
- * Esta clase representa operaciones aritméticas básicas.
+ 
  */
+
 public class aritmetica {
 
-
+    /**
+     * 
+     *Suma dos números enteros.
+     * @param a El primer número entero.
+     * @param b El segundo número entero.
+     * @return La suma de los dos números enteros.
+     */
     public int sumar(int a, int b) {
         return a + b;
     }
 
-
+    /**
+     * 
+     *Resta dos números enteros.
+     * @param a El número entero del que se restará el segundo número.
+     * @param b El número entero que se restará de a.
+     * @return La el resultado de restar los  números enteros.
+     */
     public int restar(int a, int b) {
         return a - b;
     }
 
+    /**
+     * 
+     *Multiplica dos números enteros.
+     * @param a El primer número entero.
+     * @param b El segundo número entero.
+     * @return El resultado de la multiplicación de los dos números enteros.
+     */
     public int multiplicar(int a, int b) {
         return a * b;
     }
 
+    /**
+     * Divide dos números enteros.
+     *
+     * @param a El dividendo.
+     * @param b El divisor.
+     * @return El resultado de la división
+     * @throws IllegalArgumentException Si el divisor es cero.
+     */
     public double dividir(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("No se puede dividir por cero");
@@ -29,4 +57,3 @@ public class aritmetica {
         return (double) a / b;
     }
 }
-
--- src/javadoca/operaciones.java
+++ src/javadoca/operaciones.java
@@ -7,6 +7,12 @@ package javadoca;
  * Esta clase representa operaciones matemáticas avanzadas.
  */
 public class operaciones {
+    /* funcion para dividir dos numeros
+     * @param a primero numero a dividir
+     * @param b segundo numero a dividir
+     * @return el cociente de la division entre a y b
+     * @throws IllegalArgumentException si el divisor es 0
+     */
 
     public int modulo(int a, int b) {
         if (b == 0) {
@@ -15,11 +21,18 @@ public class operaciones {
         return a % b;
     }
 
- 
+ /* funcion para calcular el cuadrado de un numero
+  * @param a el numero a calcular
+  @return el resultado de multiplicar el numero por si mismo
+  */
     public int calcularCuadrado(int a) {
         return a * a;
     }
-
+/* funcion para calcular la raiz cuadrada de un numero
+ * @param a numero a calcular
+ * @throw IllegalArgumentException si el numero es negativo
+ * return la raiz cuadrada del numero
+ */
     public double calcularRaizCuadrada(double a) {
         if (a < 0) {
             throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
